9b816f98750eafb9b77cf176a4187cb3a614e124
DINDA RINDU PRASTYA <dindarindu62@gmail.com>
2026-06-15 14:33:17 +0700

    [Tahap 3] Memperbaiki struktur dengan menambahkan folder koneksi

--- koneksi/database.php
+++ koneksi/database.php
@@ -1,13 +1,29 @@
  <?php
-$host     = "localhost";
-$username = "root";
-$password = "";
-$database = "db_latihan_pbo_ti1c_dindarinduprastya";
 
-$koneksi = mysqli_connect($host, $username, $password, $database);
+class Database {
+    // Properti untuk menyimpan konfigurasi database
+    private $host     = "localhost";
+    private $username = "root";     // Sesuaikan dengan username MySQL kamu
+    private $password = "";         // Sesuaikan dengan password MySQL kamu
+    private $db_name  = "db_latihan_pbo_ti1c_dindarinduprastya";
+    public  $connection;
 
-// Memeriksa koneksi
-if (!$koneksi) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+    // Konstruktor: Otomatis berjalan saat objek Class Database dibuat
+    public function __construct() {
+        // Membuat koneksi ke MySQL menggunakan OOP mysqli
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+
+        // Memeriksa apakah koneksi mengalami error
+        if ($this->connection->connect_error) {
+            die("Koneksi database gagal: " . $this->connection->connect_error);
+        } else {
+            // Menampilkan pesan sukses jika berhasil terhubung
+            echo "<div style='color: green; font-weight: bold;'>Pesan: Sukses terhubung ke basis data db_latihan_pbo_ti1c_dindarinduprastya!</div><br>";
+        }
+    }
 }
+
+// Cara inisialisasi untuk tes koneksi langsung (bisa dihapus/dikomentari jika di-include ke file lain)
+$db = new Database();
+
 ?>
